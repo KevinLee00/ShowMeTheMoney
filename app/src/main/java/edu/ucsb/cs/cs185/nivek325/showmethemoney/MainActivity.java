7aa901c30c52ec5059a484bd85b9956449d0c37d
jimmy le <lejimmyl@yahoo.com>
2017-03-06 20:22:15 -0800

    Add tab layout

--- app/src/main/java/edu/ucsb/cs/cs185/nivek325/showmethemoney/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/nivek325/showmethemoney/MainActivity.java
@@ -1,21 +1,20 @@
 package edu.ucsb.cs.cs185.nivek325.showmethemoney;
 
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -49,7 +48,10 @@ public class MainActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setCurrentItem(1);
 
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(mViewPager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -147,11 +149,11 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Transaction History";
                 case 1:
-                    return "SECTION 2";
+                    return "Summary";
                 case 2:
-                    return "SECTION 3";
+                    return "Schedule Transactions";
             }
             return null;
         }
