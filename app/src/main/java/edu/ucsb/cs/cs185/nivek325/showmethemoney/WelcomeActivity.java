6b8def9734669a30d47acd991613fbc28990d733
jimmy le <lejimmyl@yahoo.com>
2017-03-20 20:09:41 -0700

    Change edit text styles

--- app/src/main/java/edu/ucsb/cs/cs185/nivek325/showmethemoney/WelcomeActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/nivek325/showmethemoney/WelcomeActivity.java
@@ -2,10 +2,10 @@ package edu.ucsb.cs.cs185.nivek325.showmethemoney;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
 
@@ -18,6 +18,8 @@ public class WelcomeActivity extends AppCompatActivity {
         final Intent intent = new Intent(this, MainActivity.class);
 
         final MaterialEditText editText = (MaterialEditText) findViewById(R.id.name_edit_text);
+        editText.setTextColor(ContextCompat.getColor(this, R.color.white));
+
         Button button = (Button) findViewById(R.id.welcome_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
