f3c08aa8cdf6e7de0a0f8f63f6ab832d43b83518
jimmy le <lejimmyl@yahoo.com>
2017-03-20 21:45:02 -0700

    Show changes on removing transactions for main progress bar

--- app/src/main/java/edu/ucsb/cs/cs185/nivek325/showmethemoney/PaymentManager.java
+++ app/src/main/java/edu/ucsb/cs/cs185/nivek325/showmethemoney/PaymentManager.java
@@ -41,6 +41,7 @@ public class PaymentManager {
     public static void clear(){
         while(payments.size()>0)payments.remove(0);
         listener.onAddPayment();
+        MainActivity.progressBarFragment.updateFragment();
     }
 
     public static void updateEvents(List<Event> events) {
